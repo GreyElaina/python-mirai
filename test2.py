0144e88abbc6ea938fef592259c5df04b4e97950
GreyElaina <31543961+GreyElaina@users.noreply.github.com>
2020-03-16 18:30:21 +0800

    released: v0.2.13

--- test2.py
+++ test2.py
@@ -11,12 +11,6 @@ app = Mirai(f"mirai://localhost:8070/?authKey={authKey}&qq={qq}", websocket=True
 async def event_gm(app: Mirai, message: MessageChain, group: Group, member: Member):
     if group.id == 655057127:
         debug(message)
-        await app.sendGroupMessage(
-            group.id,
-            [
-                Image.fromFileSystem()
-            ]
-        )
 
 if __name__ == "__main__":
     app.run()
