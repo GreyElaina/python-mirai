015b3199fe0e41cca4e8aacec22a500539d68c44
GreyElaina <31543961+GreyElaina@users.noreply.github.com>
2020-03-19 12:23:52 +0800

    improve: builtin dependencies name

--- mirai/utilles/dependencies.py
+++ mirai/utilles/dependencies.py
@@ -1,28 +1,34 @@
+""" python-mirai 自带的一些小型依赖注入设施.
+
+各个函数皆返回 mirai.Depend 实例, 不需要进一步的包装.
+
+"""
+
 from mirai.depend import Depend
 from mirai import MessageChain, Cancelled, Image, Mirai, At
 import re
 from typing import List
 
-def regex(pattern):
+def Regex(pattern):
     async def regex_depend_wrapper(message: MessageChain):
         if not re.match(pattern, message.toString()):
             raise Cancelled
     return Depend(regex_depend_wrapper)
 
-def startswith(string):
+def StartsWith(string):
     async def startswith_wrapper(message: MessageChain):
         if not message.toString().startswith(string):
             raise Cancelled
     return Depend(startswith_wrapper)
 
-def photo(num=1):
+def WithPhoto(num=1):
     "断言消息中图片的数量"
     async def photo_wrapper(message: MessageChain):
         if len(message.getAllofComponent(Image)) < num:
             raise Cancelled
     return Depend(photo_wrapper)
 
-def at(qq=None):
+def AssertAt(qq=None):
     "判断是否at了某人, 如果没有则判断是否at了机器人"
     async def at_wrapper(app: Mirai, message: MessageChain):
         at_set: List[At] = message.getAllofComponent(At)
