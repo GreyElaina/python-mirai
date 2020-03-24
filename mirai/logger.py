56d7f944f9d3755779d8ab80af0eff1d08dc9957
GreyElaina <31543961+GreyElaina@users.noreply.github.com>
2020-03-24 17:13:44 +0800

    add: debug method

--- mirai/logger.py
+++ mirai/logger.py
@@ -3,9 +3,10 @@ from logbook import (
     INFO,
     DEBUG
 )
+import os
 import sys
 
-stream_handler = StreamHandler(sys.stdout, level=INFO)
+stream_handler = StreamHandler(sys.stdout, level=INFO if not os.environ.get("MIRAI_DEBUG") else DEBUG)
 stream_handler.format_string = '[{record.time:%Y-%m-%d %H:%M:%S}][Mirai] {record.level_name}: {record.channel}: {record.message}'
 stream_handler.push_application()
 
